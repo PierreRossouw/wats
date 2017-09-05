1e717a55d6bfba6355386a7d76b8acd98e12d32c
Pierre Rossouw <PierreRossouw@users.noreply.github.com>
2017-09-05 17:39:48 +1000

    Update example.rs

--- example.rs
+++ example.rs
@@ -1,9 +1,10 @@
+// Count the number of primes in the range 0-100000
 pub fn main() -> i32 {
-  let primes: i32 = 0;
-  let range: i32 = 100000;
+  let mut primes: i32 = 0;
+  let mut range: i32 = 100000;
   while range > 1 {
-    let j: i32 = 2;
-    let is_prime: bool = true
+    let mut j: i32 = 2;
+    let mut is_prime: bool = true
     while j < range {
       if range % j == 0 {
         is_prime = false;
