b15e52999a74c5260f4cd7b16def4827a9038fc9
Pierre Rossouw <PierreRossouw@users.noreply.github.com>
2019-07-21 17:05:46 +1000

    Update example.rs

--- example.rs
+++ example.rs
@@ -1,21 +1,21 @@
-// Count the number of primes in the range 0-100000
-pub fn main() -> i32 {
-  let mut primes: i32 = 0;
-  let mut range: i32 = 100000;
+;; Count the number of primes in the range 0-100000
+export func $main() i32 {
+  local mut $primes i32 = 0
+  local mut $range i32 = 100000
   
-  while range > 1 {
-    let mut j: i32 = 2;
-    let mut is_prime: bool = true;
-    while j < range {
-      if range % j == 0 {
-        is_prime = false;
-        break;
+  loop { br_if $range <= 0 
+    local mut $j i32 = 2
+    local mut $is_prime i32 = 1
+    loop { br_if $j >= $range 
+      if $range % $j == 0 {
+        $is_prime = 0
+        br
       }
-      j += 1;
+      $j += 1
     }
-    primes += is_prime;
-    range -= 1;
+    $primes += $is_prime
+    $range -= 1
   }
   
-  primes
+  $primes
 }
